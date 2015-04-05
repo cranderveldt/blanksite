bc31ada5131a2a724eb8ff16316a3cd8155f346d
cranderveldt <djnaphouse@gmail.com>
2015-04-04 21:14:46 -0400

    two space tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,30 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        
-        sass: {
-            dev: {
-                options: {
-                    style: 'compressed'
-                },
-                files : {
-                    'css/styles.css':'sass/styles.scss'
-                }
-            }
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    sass: {
+      dev: {
+        options: {
+          style: 'compressed'
         },
-        watch: {
-            sass: {
-                files: ['sass/*.scss'],
-                tasks: ['sass:dev']
-            }
+        files : {
+          'css/styles.css':'sass/styles.scss'
         }
-    });
+      }
+    },
+    watch: {
+      sass: {
+        files: ['sass/*.scss'],
+        tasks: ['sass:dev']
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    // Default task(s).
-    grunt.registerTask('default', ['sass:dev','watch:sass']);
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  // Default task(s).
+  grunt.registerTask('default', ['sass:dev','watch:sass']);
 
 };
